bb4de8de95f76283e4f1939d907a37d4797fea0b
Md Arifur Rahman <57655164+arifur946954@users.noreply.github.com>
2024-10-24 18:16:00 +0600

    sorting array

---
+++ MediumArray.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class MediumArray {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please Size array");
+        int size= scanner.nextInt();
+Integer[] arIn=new Integer[size];
+for (int i=0;i<size;i++){
+
+}
+
+    }
+}
---
+++ SortArray.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class SortArray {
+    public static void main(String[] args) {
+        int[] str={};
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Please input the length of array");
+        int size=scanner.nextInt();
+
+        Scanner scanner1=new Scanner(System.in);
+        System.out.println("pls insert the array of string");
+        int[] numbers=new int[size];
+        char
+
+        for (int i=0;i<size;i++){
+
+        }
+
+
+    }
+}
