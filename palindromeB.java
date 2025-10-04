a6f6b0c18b50c077491246e1682e6a00dc018165
Md Arifur Rahman <57655164+arifur946954@users.noreply.github.com>
2025-10-04 12:25:24 +0600

    palindrome space and int

--- palindromeB.java
+++ palindromeB.java
@@ -1,7 +1,10 @@
 public class palindromeB {
     public static void main(String[] args) {
-        String vl="Aba";
-        String value=vl.toLowerCase();
+        String v1=" a b a  Aba";
+        String vl=v1.replaceAll("\\s+", "");//remove all space every where
+        //String vl=v1.replaceAll("^\\s+", "");//remove  space only start position
+
+        String value=vl.toLowerCase();//for case sensetive
         String Reversed="";
         int len=value.length()-1;
         for (int i = len; i >= 0; i--) {
